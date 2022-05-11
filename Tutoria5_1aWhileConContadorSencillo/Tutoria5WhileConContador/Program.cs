7757fb1de7c6e839183ea2ae7f1d2fb3785bbda9
Katherine010 <TC21005@ues.edu.sv>
2022-05-11 10:40:26 -0600

    Commit

---
+++ Tutoria5_1aWhileConContadorSencillo/Tutoria5WhileConContador/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tutoria5WhileConContador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double NumeroLeido, Suma, Promedio;
+            int Contador;
+            Suma = 0;
+            Contador=1;
+            while (Contador<=5)
+            {
+                Console.WriteLine("Ingrese el numero: " + Contador);
+                NumeroLeido = double.Parse(Console.ReadLine());
+                Suma = Suma + NumeroLeido;
+                Contador = Contador + 1;
+            }
+            Promedio = Suma / (Contador-1);
+            Console.WriteLine("El Promedio es: " + Promedio);
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
